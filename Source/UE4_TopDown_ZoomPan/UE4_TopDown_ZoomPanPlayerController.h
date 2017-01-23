848d05c24832f12e151f33aa476c2af497339c81
rockhowse <rockhowse@gmail.com>
2017-01-22 23:53:32 -0500

    removed existing 'click to move' code from the PlayerController

--- Source/UE4_TopDown_ZoomPan/UE4_TopDown_ZoomPanPlayerController.h
+++ Source/UE4_TopDown_ZoomPan/UE4_TopDown_ZoomPanPlayerController.h
@@ -12,29 +12,11 @@ public:
 	AUE4_TopDown_ZoomPanPlayerController();
 
 protected:
-	/** True if the controlled character should navigate to the mouse cursor. */
-	uint32 bMoveToMouseCursor : 1;
 
 	// Begin PlayerController interface
 	virtual void PlayerTick(float DeltaTime) override;
 	virtual void SetupInputComponent() override;
 	// End PlayerController interface
-
-	/** Resets HMD orientation in VR. */
-	void OnResetVR();
-
-	/** Navigate player to the current mouse cursor location. */
-	void MoveToMouseCursor();
-
-	/** Navigate player to the current touch location. */
-	void MoveToTouchLocation(const ETouchIndex::Type FingerIndex, const FVector Location);
-	
-	/** Navigate player to the given world location. */
-	void SetNewMoveDestination(const FVector DestLocation);
-
-	/** Input handlers for SetDestination action. */
-	void OnSetDestinationPressed();
-	void OnSetDestinationReleased();
 };
 
 
