87f3648db839464dcb69ccd3c742fd9feb13ed38
rockhowse <rockhowse@gmail.com>
2017-01-22 23:56:58 -0500

    added replacement input handlers that will map to our input configuration

--- Source/UE4_TopDown_ZoomPan/UE4_TopDown_ZoomPanPlayerController.cpp
+++ Source/UE4_TopDown_ZoomPan/UE4_TopDown_ZoomPanPlayerController.cpp
@@ -2,6 +2,7 @@
 
 #include "UE4_TopDown_ZoomPan.h"
 #include "UE4_TopDown_ZoomPanPlayerController.h"
+#include "Engine/LocalPlayer.h"
 #include "AI/Navigation/NavigationSystem.h"
 #include "Runtime/Engine/Classes/Components/DecalComponent.h"
 #include "Kismet/HeadMountedDisplayFunctionLibrary.h"
@@ -21,4 +22,93 @@ void AUE4_TopDown_ZoomPanPlayerController::SetupInputComponent()
 {
 	// set up gameplay key bindings
 	Super::SetupInputComponent();
+
+	// Input Handlers
+	InputComponent->BindAxis("MoveForward", this, &AUE4_TopDown_ZoomPanPlayerController::OnMoveForwardAxis);
+	InputComponent->BindAxis("MoveRight", this, &AUE4_TopDown_ZoomPanPlayerController::OnMoveRightAxis);
+	InputComponent->BindAxis("MouseHorizontal", this, &AUE4_TopDown_ZoomPanPlayerController::OnMouseHorizontal);
+	InputComponent->BindAxis("MouseVertical", this, &AUE4_TopDown_ZoomPanPlayerController::OnMouseVertical);
+	InputComponent->BindAction("ZoomIn", EInputEvent::IE_Pressed, this, &AUE4_TopDown_ZoomPanPlayerController::OnZoomInAction);
+	InputComponent->BindAction("ZoomOut", EInputEvent::IE_Pressed, this, &AUE4_TopDown_ZoomPanPlayerController::OnZoomOutAction);
+	InputComponent->BindAction("LookAround", EInputEvent::IE_Pressed, this, &AUE4_TopDown_ZoomPanPlayerController::OnLookAroundStart);
+	InputComponent->BindAction("LookAround", EInputEvent::IE_Released, this, &AUE4_TopDown_ZoomPanPlayerController::OnLookAroundStop);
+}
+
+void AUE4_TopDown_ZoomPanPlayerController::OnMoveForwardAxis(float axisValue)
+{
+	APawn* const Pawn = GetPawn();
+	AUE4_TopDown_ZoomPanCharacter* character = Cast<AUE4_TopDown_ZoomPanCharacter>(Pawn);
+	if (character)
+	{
+		character->MoveCharacterForward(axisValue);
+	}
+}
+
+void AUE4_TopDown_ZoomPanPlayerController::OnMoveRightAxis(float axisValue)
+{
+	APawn* const Pawn = GetPawn();
+	AUE4_TopDown_ZoomPanCharacter* character = Cast<AUE4_TopDown_ZoomPanCharacter>(Pawn);
+	if (character)
+	{
+		character->MoveCharacterRight(axisValue);
+	}
+}
+
+void AUE4_TopDown_ZoomPanPlayerController::OnMouseHorizontal(float axisValue)
+{
+	if (lookAroundEnabled)
+	{
+		APawn* const Pawn = GetPawn();
+		Pawn->AddControllerYawInput(axisValue);
+		Cast<ULocalPlayer>(Player)->ViewportClient->Viewport->SetMouse(mouseLockPositionX, mouseLockPositionY);
+	}
+}
+void AUE4_TopDown_ZoomPanPlayerController::OnMouseVertical(float axisValue)
+{
+	if (lookAroundEnabled)
+	{
+		APawn* const Pawn = GetPawn();
+		AUE4_TopDown_ZoomPanCharacter* character = Cast<AUE4_TopDown_ZoomPanCharacter>(Pawn);
+		if (character)
+		{
+			character->RotateCameraArm(FRotator(axisValue, 0.0f, 0.0f));
+		}
+		Cast<ULocalPlayer>(Player)->ViewportClient->Viewport->SetMouse(mouseLockPositionX, mouseLockPositionY);
+	}
+}
+
+void AUE4_TopDown_ZoomPanPlayerController::OnZoomInAction()
+{
+	APawn* const Pawn = GetPawn();
+	AUE4_TopDown_ZoomPanCharacter* character = Cast<AUE4_TopDown_ZoomPanCharacter>(Pawn);
+	if (character)
+	{
+		character->ChangeCameraArmLength(-1.0f);
+	}
+}
+
+void AUE4_TopDown_ZoomPanPlayerController::OnZoomOutAction()
+{
+	APawn* const Pawn = GetPawn();
+	AUE4_TopDown_ZoomPanCharacter* character = Cast<AUE4_TopDown_ZoomPanCharacter>(Pawn);
+	if (character)
+	{
+		character->ChangeCameraArmLength(1.0f);
+	}
+}
+
+void AUE4_TopDown_ZoomPanPlayerController::OnLookAroundStart()
+{
+	//Lock mouse cursor
+	lookAroundEnabled = true;
+	bShowMouseCursor = false;
+	mouseLockPositionX = Cast<ULocalPlayer>(Player)->ViewportClient->Viewport->GetMouseX();
+	mouseLockPositionY = Cast<ULocalPlayer>(Player)->ViewportClient->Viewport->GetMouseY();
+}
+
+void AUE4_TopDown_ZoomPanPlayerController::OnLookAroundStop()
+{
+	//Unlock mouse cursor
+	lookAroundEnabled = false;
+	bShowMouseCursor = true;
 }
--- Source/UE4_TopDown_ZoomPan/UE4_TopDown_ZoomPanPlayerController.h
+++ Source/UE4_TopDown_ZoomPan/UE4_TopDown_ZoomPanPlayerController.h
@@ -11,12 +11,27 @@ class AUE4_TopDown_ZoomPanPlayerController : public APlayerController
 public:
 	AUE4_TopDown_ZoomPanPlayerController();
 
+	// Input handlers
+	void OnMoveForwardAxis(float axisValue);
+	void OnMoveRightAxis(float axisValue);
+	void OnMouseHorizontal(float axisValue);
+	void OnMouseVertical(float axisValue);
+	void OnZoomInAction();
+	void OnZoomOutAction();
+	void OnLookAroundStart();
+	void OnLookAroundStop();
+
 protected:
 
 	// Begin PlayerController interface
 	virtual void PlayerTick(float DeltaTime) override;
 	virtual void SetupInputComponent() override;
 	// End PlayerController interface
+
+private:
+	bool lookAroundEnabled = false;
+	int32 mouseLockPositionX;
+	int32 mouseLockPositionY;
 };
 
 
